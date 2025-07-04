b59070a7728cf32ce9b3a361ae9adbba7c341e64
danielpappa <daniel.pappalardo@icloud.com>
2025-07-04 08:22:14 +0200

    Initial commit

---
+++ Bouli/BouliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BouliApp.swift
+//  Bouli
+//
+//  Created by Daniel Pappalardo on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BouliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bouli/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bouli
+//
+//  Created by Daniel Pappalardo on 04/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
